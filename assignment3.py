1f30e332128de07bffa83b1c965710a9acf0e43a
Bradley Shrader <bkshrader@bsu.edu>
2019-10-08 19:36:03 -0400

    Add project boilerplate

---
+++ assignment3.py
@@ -0,0 +1,40 @@
+import networkx
+import matplotlib
+
+
+def make_directories_if_missing():
+    pass
+
+
+def plot_bar():
+    pass
+
+
+def plot_pie():
+    pass
+
+
+def plot_line():
+    pass
+
+
+def plot_scatter():
+    pass
+
+
+def plot_histogram():
+    pass
+
+
+def plot_network():
+    pass
+
+
+if __name__ == "__main__":
+    make_directories_if_missing()
+    plot_bar()
+    plot_pie()
+    plot_line()
+    plot_scatter()
+    plot_histogram()
+    plot_network()
