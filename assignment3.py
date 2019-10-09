6832413ce5901d45d4b1720ca579b83077d8bb09
Bradley Shrader <bkshrader@bsu.edu>
2019-10-08 20:56:08 -0400

    Process fire data for bar chart

--- assignment3.py
+++ assignment3.py
@@ -1,22 +1,31 @@
 import networkx
-import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 from sodapy import Socrata
 
 NEW_YORK_DATA_AUTH_TOKEN = "QwLSoW0YRtDKSHtGNty0L7b8G"
+NEW_YORK_DATA_CLIENT = None
 FIRE_RESOURCE_NAME = "8m42-w767"
+FIRE_DATAFRAME = None
 
 
 def load_data():
     # Load Fire Incident Dispatch Data
     global NEW_YORK_DATA_CLIENT, FIRE_DATAFRAME
     NEW_YORK_DATA_CLIENT = Socrata("data.cityofnewyork.us", NEW_YORK_DATA_AUTH_TOKEN)
-    firedata = NEW_YORK_DATA_CLIENT.get(FIRE_RESOURCE_NAME, limit=10000)
+    firedata = NEW_YORK_DATA_CLIENT.get(FIRE_RESOURCE_NAME, limit=100)  # TODO: Replace with 10000 after testing
     FIRE_DATAFRAME = pd.DataFrame.from_records(firedata)
 
 
 def plot_bar():
-    pass
+    # Data Analysis: Find average response time for each borough
+    boroughs = FIRE_DATAFRAME.alarm_box_borough.unique()
+    average_response_times = dict()
+    for borough in boroughs:
+        borough_incidents = FIRE_DATAFRAME.loc[(FIRE_DATAFRAME["alarm_box_borough"] == borough) & (FIRE_DATAFRAME["valid_incident_rspns_time_indc"] == "Y")]
+        borough_average_response_time = borough_incidents["incident_response_seconds_qy"].astype(float).mean()
+        average_response_times[borough] = borough_average_response_time
+
 
 
 def plot_pie():
