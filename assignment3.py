ca9dba062af39e48d5872e4dd94c188f9a61c25d
Bradley Shrader <bkshrader@bsu.edu>
2019-10-14 14:26:57 -0400

    Fix issue with bar chart only plotting a couple days worth of data
    Changed query to only get data from previous year that has valid time

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,9 @@
 import networkx
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from sodapy import Socrata
+from datetime import datetime
 
 NEW_YORK_DATA_AUTH_TOKEN = "QwLSoW0YRtDKSHtGNty0L7b8G"
 NEW_YORK_DATA_CLIENT = Socrata("data.cityofnewyork.us", NEW_YORK_DATA_AUTH_TOKEN)
@@ -10,22 +12,32 @@ FIRE_RESOURCE_NAME = "8m42-w767"
 
 def plot_bar():
     # Ingest: Load Fire Incident Dispatch Data
-    data = NEW_YORK_DATA_CLIENT.get(FIRE_RESOURCE_NAME, limit=100000)  # TODO: Replace with 10000 after testing
+    query = f"""
+    select 
+        incident_datetime, 
+        alarm_box_borough, 
+        incident_response_seconds_qy
+    where
+        valid_incident_rspns_time_indc = 'Y' and
+        date_extract_y(incident_datetime) = {datetime.now().year - 1}
+    limit 2000000
+    """
+    data = NEW_YORK_DATA_CLIENT.get(FIRE_RESOURCE_NAME, query=query)
     firedata = pd.DataFrame.from_records(data)
+    print(firedata)
     # Data Analysis: Find average response time for each borough
     boroughs = firedata.alarm_box_borough.unique()
     average_response_times = dict()
     response_times_std = dict()
     for borough in boroughs:
-        borough_incidents = firedata.loc[(firedata["alarm_box_borough"] == borough) & (
-                firedata["valid_incident_rspns_time_indc"] == "Y")]
+        borough_incidents = firedata.loc[firedata["alarm_box_borough"] == borough]
         borough_average_response_time = borough_incidents["incident_response_seconds_qy"].astype(float).mean()
         borough_response_time_std = borough_incidents["incident_response_seconds_qy"].astype(float).std()
         average_response_times[borough] = borough_average_response_time
         response_times_std[borough] = borough_response_time_std
     # Data Visualization
     plt.figure(figsize=(10, 6))
-    plt.title("Fire Response Time by Borough in NYC")
+    plt.title(f"Fire Response Time by Borough in NYC\n({firedata.incident_datetime.astype('datetime64').min():%B %Y} - {firedata.incident_datetime.astype('datetime64').max():%B %Y})")
     plt.ylabel("Average Incident Response Time (seconds)")
     plt.xlabel("Borough Name")
     plt.bar(boroughs, average_response_times.values(), yerr=response_times_std.values(), color="bgrcmyk")
@@ -39,11 +51,6 @@ def plot_pie():
 
 
 def plot_line():
-    # dates = firedata["incident_datetime"].astype("datetime64").dt.date.unique()
-    # for date in dates:
-    #     incidents = firedata.loc[firedata["incident_datetime"].astype("datetime64").dt.date == date]
-    #     incident_count = len(incidents.index)
-    #     print(date, incident_count)
     pass
 
 
